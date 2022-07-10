a28380dc4449ecc2b6d3ef95c230dfca57a62209
mynamevarun <43521927+mynamevarun@users.noreply.github.com>
2022-07-10 21:50:52 +0530

    Update TruncateString.js

--- TruncateString.js
+++ TruncateString.js
@@ -1,9 +1,9 @@
-function truncateString(givenString, length) {
+function truncateString(givenString, length, charactor) {
 	if (givenString.length > length) {
 		if (length <= 3) {
-			return givenString.slice(0, length - 3) + "...";
+			return givenString.slice(0, length - 3) + charactor ? charactor : "...";
 		} else {
-         return givenString.slice(0, length) + "...";
+         return givenString.slice(0, length) + charactor ? charactor : "...";
 		}
 	} else {
       return givenString;
